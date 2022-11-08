2e0eab0bccd44b743b6a5f30b32dcf97d426ddd8
Nandon <nandonarts@gmail.com>
2022-11-08 18:18:50 -0400

    current state of the assignemnt

---
+++ app.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded',(event)=>{
+
+    let sub =document.querySelector("button")
+    sub.onclick= function(){Act()}
+    
+    function Act(){
+        fetch('http://localhost/comp2245-assignment4/index.html')
+            .then(response =>  response.text())
+            .then(data =>{
+                alert(data)
+            })
+
+        .catch(error =>alert(error))
+        console.log("check 1");
+    };
+
+});
